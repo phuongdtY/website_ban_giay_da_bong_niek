e4710dc04025aafb82de66b2f28794c150e3f3bd
canhdv281 <canhdat28@gmail.com>
2023-06-24 17:48:59 +0700

    tao khung project

---
+++ src/main/java/com/poly/niek/NiekApplication.java
@@ -0,0 +1,13 @@
+package com.poly.niek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NiekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NiekApplication.class, args);
+    }
+
+}
