f55b24f2f6ea85e788ce843aa5dd6a9a7e70c436
canhdv281 <canhdat28@gmail.com>
2023-07-02 16:19:30 +0700

    up dependency

---
+++ src/main/java/com/poly/niek/entity/MauSac.java
@@ -0,0 +1,16 @@
+package com.poly.niek.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class MauSac {
+    
+}
